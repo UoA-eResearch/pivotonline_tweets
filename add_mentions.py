a46b57027c65f52babf1f5098ab195729e46a0ac
Nick Young <nick.young@auckland.ac.nz>
2020-05-01 11:30:55 +1200

    fix @mentions

--- add_mentions.py
+++ add_mentions.py
@@ -11,7 +11,7 @@ for i, tweet in enumerate(tqdm(tweets["features"])):
         screen_name = other_tweet["properties"]["screen_name"]
         if screen_name == tweet["properties"]["screen_name"]: # self mention
             continue
-        if screen_name in tweet["properties"]["text"]:
+        if "@" + screen_name in tweet["properties"]["text"]:
             if "mentions" not in tweets["features"][i]["properties"]:
                 tweets["features"][i]["properties"]["mentions"] = []
             if screen_name not in tweets["features"][i]["properties"]["mentions"]:
